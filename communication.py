43a16ceb8521e92fbe88b9ce1d941627dbfadf7e
bblwbtd <gmyxgmyx@outlook.com>
2020-03-29 14:23:23 +0800

    add header

--- communication.py
+++ communication.py
@@ -1,48 +1,56 @@
 import requests
-from config import minecraft_server_address
+
+from config import minecraft_server_host
+
+header = {
+    'Host': minecraft_server_host,
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br'
+}
 
 
 def check_user_from_server(username, password) -> bool:
-    response = requests.get(f'{minecraft_server_address}/user/login', {
+    response = requests.get(f'https://{minecraft_server_host}/user/login', {
         'username': username,
         'password': password
-    })
+    }, headers=header)
     body = response.text
     return body == "success"
 
 
 def add_user_to_server(username, password):
     # invitor不发往后端，是作为奖励传达的。.
-    response = requests.post(f'{minecraft_server_address}/user/register', params={
+    response = requests.post(f'https://{minecraft_server_host}/user/register', params={
         'username': username,
         'password': password
-    })
+    }, headers=header)
     return response.text == "success"
 
 
 def change_user_password_from_server(username, original_password, new_password):
-    response = requests.post(f'{minecraft_server_address}/user/password/change', params={
+    response = requests.post(f'https://{minecraft_server_host}/user/password/change', params={
         'username': username,
         'old_password': original_password,
         'new_password': new_password
-    })
+    }, headers=header)
     return response.text == "success"
 
 
 def del_user_from_server(username):
-    response = requests.post(f'{minecraft_server_address}/user/delete', params={
+    response = requests.post(f'https://{minecraft_server_host}/user/delete', params={
         'username': username
-    })
+    }, headers=header)
     return response.text == "success"
 
 
 def get_user_list_from_server():
-    return requests.get(f'{minecraft_server_address}/user/all').json()
+    return requests.get(f'https://{minecraft_server_host}/user/all', headers=header).json()
 
 
 def give_kit(username, kit_name):
-    response = requests.post(f'{minecraft_server_address}/kit/give', params={
+    response = requests.post(f'https://{minecraft_server_host}/kit/give', params={
         'username': username,
         'name': kit_name
-    })
+    }, headers=header)
     return response.text == 'success'
